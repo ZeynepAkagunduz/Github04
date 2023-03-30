73fdacd8fa54f20cbb647780eb0d91ef3eaf4750
ZeynepAkagunduz <zeynepakagunduz06@gmail.com>
2023-03-30 09:04:47 +0300

    4.gün

---
+++ src/C02.java
@@ -0,0 +1,23 @@
+public class C02 {
+    /*
+    Git-Github'da projelerimizin üzerinde çalışırken temelde 3 adımda işlem yapıyoruz
+        1- Projelerimizi/Dosyalarımızı git ile ilişkilendiriyoruz
+                git init
+        2- projemizde/dosyamızda bulunnanları versiyon oluşturuyorduk
+            git add .
+            git commit -m "versiyonadi"
+        3- github'a yollama/entegrasyonu
+            git remote add origin repoUrlAdresi
+            already origin
+            git remote set-url repoUrlAdresi
+            git push
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("İşlem ekleyelim");
+        System.out.println("HEAD");
+        System.out.println("Master");
+
+    }
+}
